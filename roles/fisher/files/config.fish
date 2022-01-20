31aa07fe8a77a8a11bc6049afe01fafd5fda8c0c
Craig Anderson <1877372+CDA0@users.noreply.github.com>
2022-01-20 10:45:51 +0000

    just a few fixes and additions

--- roles/fisher/files/config.fish
+++ roles/fisher/files/config.fish
@@ -1,15 +1,17 @@
 # Managed by ansibile
 
-if status is-interactive
-and not set -q TMUX
+# if status is-interactive
+# and not set -q TMUX
+#
+#  set -g TMUX tmux new-session -d -s base
+#  eval $TMUX
+#  tmux attach-session -d -t base
+#
+# end
 
-  set -g TMUX tmux new-session -d -s base
-  eval $TMUX
-  tmux attach-session -d -t base
-
-  # Work specific config
-  if test -f $HOME/.config/fish/local.fish
-    source $HOME/.config/fish/local.fish
-  end
-
+# Work specific config
+if test -f $HOME/.config/fish/local.fish
+  source $HOME/.config/fish/local.fish
 end
+
+fish_add_path $HOME/.local/bin/
